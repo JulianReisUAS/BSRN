e1c8ce976b6861fe5afd5cc8c4fff3e6503e0c68
JulianReisUAS <reisjulian6@gmail.com>
2023-06-23 19:33:25 +0200

    final

--- client.c
+++ client.c
@@ -1,12 +1,138 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+#define MAX_MESSAGE_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
 
 int main() {
-    char eingabe [100]; // Array für die Eingabe
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[MAX_MESSAGE_SIZE];
 
-    printf("Bitte erstes Wort eingeben: ");
-    scanf("%s", eingabe); 
+    // Eingabe der Übertragungsart, Servernamen und Dienstnamen
+    char transport[10], server_name[16], service_name[16];
+    int valid_input = 0;
+    while (!valid_input) {
+        printf("Welche Übertragungsart möchten Sie verwenden? (TCP/UDP): ");
+        scanf("%s", transport);
+        printf("Geben Sie den Servernamen ein ('srv' für 127.0.0.1): ");
+        scanf("%s", server_name);
+        printf("Geben Sie den gemeinsamen Dienstnamen ein: ");
+        scanf("%s", service_name);
 
-    printf("Ausgabe: %s \n", eingabe);
+        // Serviceinformationen abrufen
+        struct servent *service;
+        if (strcmp(transport, "TCP") == 0) {
+            service = getservbyname(service_name, "tcp");
+        } else if (strcmp(transport, "UDP") == 0) {
+            service = getservbyname(service_name, "udp");
+        } else {
+            fprintf(stderr, "Fehler: Ungültige Übertragungsart\n");
+            continue;
+        }
 
+        if (service == NULL) {
+            fprintf(stderr, "Fehler: Unbekannter Dienst\n");
+            continue;
+        }
+
+        portno = ntohs(service->s_port);
+
+        // Servername "srv" wurde eingegeben, verwenden Sie 127.0.0.1 als IP-Adresse
+        if (strcmp(server_name, "srv") == 0) {
+            server = gethostbyname("localhost");
+        } else {
+            server = gethostbyname(server_name);
+        }
+
+        if (server == NULL) {
+            fprintf(stderr, "Fehler: Unbekannter Server\n");
+            continue;
+        }
+
+        valid_input = 1;
+    }
+
+    // Socket erstellen
+    if (strcmp(transport, "TCP") == 0) {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    } else if (strcmp(transport, "UDP") == 0) {
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    }
+
+    if (sockfd < 0)
+        error("Fehler beim Öffnen des Sockets");
+
+    // Serveradresse initialisieren
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+
+    // Verbindung zum Server herstellen (TCP)
+    if (strcmp(transport, "TCP") == 0) {
+        if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+            error("Fehler beim Verbindungsaufbau");
+    }
+
+    // Zustandsvariable für die Client-Ausgabe
+    int isFirstMessage = 1;
+
+    // Kommunikationsschleife
+    while (1) {
+        // Nachricht vom Benutzer eingeben und an den Server senden
+        memset(buffer, 0, MAX_MESSAGE_SIZE);
+        fgets(buffer, MAX_MESSAGE_SIZE - 1, stdin);
+
+        if (strcmp(transport, "TCP") == 0) {
+            n = write(sockfd, buffer, strlen(buffer));
+        } else if (strcmp(transport, "UDP") == 0) {
+            n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+        }
+
+        if (n < 0)
+            error("Fehler beim Schreiben auf den Socket");
+
+        // Prüfen, ob Benutzer die Verbindung beenden möchte
+        if (strncmp(buffer, "bye", 3) == 0)
+            break;
+
+        // Antwort des Servers empfangen
+        memset(buffer, 0, MAX_MESSAGE_SIZE);
+        if (strcmp(transport, "TCP") == 0) {
+            n = read(sockfd, buffer, MAX_MESSAGE_SIZE - 1);
+        } else if (strcmp(transport, "UDP") == 0) {
+            socklen_t serv_len = sizeof(serv_addr);
+            n = recvfrom(sockfd, buffer, MAX_MESSAGE_SIZE - 1, 0, (struct sockaddr *)&serv_addr, &serv_len);
+        }
+
+        if (n < 0)
+            error("Fehler beim Lesen vom Socket");
+
+        // Empfangene Nachricht anzeigen
+        if (!isFirstMessage) {
+            printf("Server: %s\n", buffer);
+        }
+
+        // "Client:" nur einmal anzeigen, nachdem die Verbindung hergestellt wurde
+        isFirstMessage = 0;
+
+        // Nachricht vom Benutzer eingeben
+        printf("Client: ");
+        fflush(stdout);
+    }
+
+    // Verbindung schließen und Programm beenden
+    close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,7 +1,131 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pcap.h>
+#include <sys/socket.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hier spricht der Server");
+#define MAX_MESSAGE_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+void packetHandler(u_char *userData, const struct pcap_pkthdr *pkthdr, const u_char *packetData) {
+    printf("Packet captured\n");
+}
+
+int main() {
+    int sockfd, newsockfd, n;
+    socklen_t clilen, addrlen;
+    char buffer[MAX_MESSAGE_SIZE];
+    struct sockaddr_in serv_addr, cli_addr;
+
+    // Eingabe der Übertragungsart, des Dienstnamens und der Datei-Pfad
+    char transport[10];
+    char service_name[100];
+    char filepath[100];
+
+    // Serviceinformationen abrufen
+    struct servent *service = NULL;
+    while (1) {
+        printf("Welche Übertragungsart möchten Sie verwenden? (TCP/UDP): ");
+        scanf("%s", transport);
+
+        printf("Geben Sie den gemeinsamen Dienstnamen ein: ");
+        scanf("%s", service_name);
+
+        service = getservbyname(service_name, strcmp(transport, "TCP") == 0 ? "tcp" : "udp");
+        if (service != NULL)
+            break;
+
+        fprintf(stderr, "Fehler: Ungültige Übertragungsart oder Dienstname\n");
+    }
+
+    // Socket erstellen
+    sockfd = socket(AF_INET, strcmp(transport, "TCP") == 0 ? SOCK_STREAM : SOCK_DGRAM, 0);
+    if (sockfd < 0)
+        error("Fehler beim Öffnen des Sockets");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = service->s_port;
+
+    // Binden des Sockets an die Serveradresse
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("Fehler beim Binden");
+
+    // Das "julian" muss durch den eigenen Benutzernamen ausgetauscht werden
+    snprintf(filepath, sizeof(filepath), "/home/julian/Desktop/mitschnitt.pcap");
+
+    char command[256];
+    snprintf(command, sizeof(command), "%s -i lo %s port %d -w %s &", 
+        "tcpdump",
+        strcmp(transport, "TCP") == 0 ? "" : "udp",
+        ntohs(serv_addr.sin_port),
+        filepath
+    );
+    system(command);
+
+    int isConnectionClosed = 0;
+    if (strcmp(transport, "TCP") == 0) {
+        listen(sockfd, 5);
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("Fehler beim Akzeptieren der Verbindung");
+
+        while (!isConnectionClosed) {
+            memset(buffer, 0, MAX_MESSAGE_SIZE);
+            n = read(newsockfd, buffer, MAX_MESSAGE_SIZE - 1);
+            if (n < 0)
+                error("Fehler beim Lesen vom Socket");
+
+            printf("Client: %s\n", buffer);
+
+            if (strncmp(buffer, "bye", 3) == 0)
+                isConnectionClosed = 1;
+            else {
+                printf("Server: ");
+                memset(buffer, 0, MAX_MESSAGE_SIZE);
+                fgets(buffer, MAX_MESSAGE_SIZE - 1, stdin);
+                n = write(newsockfd, buffer, strlen(buffer));
+                if (n < 0)
+                    error("Fehler beim Schreiben auf den Socket");
+            }
+        }
+
+        close(newsockfd);
+    } else if (strcmp(transport, "UDP") == 0) {
+        while (!isConnectionClosed) {
+            memset(buffer, 0, MAX_MESSAGE_SIZE);
+            addrlen = sizeof(cli_addr);
+            n = recvfrom(sockfd, buffer, MAX_MESSAGE_SIZE - 1, 0, (struct sockaddr *) &cli_addr, &addrlen);
+            if (n < 0)
+                error("Fehler beim Lesen vom Socket");
+
+            printf("Client: %s\n", buffer);
+
+            if (strncmp(buffer, "bye", 3) == 0)
+                isConnectionClosed = 1;
+            else {
+                printf("Server: ");
+                memset(buffer, 0, MAX_MESSAGE_SIZE);
+                fgets(buffer, MAX_MESSAGE_SIZE - 1, stdin);
+                n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cli_addr, addrlen);
+                if (n < 0)
+                    error("Fehler beim Schreiben auf den Socket");
+            }
+        }
+    }
+
+    snprintf(command, sizeof(command), "pkill tcpdump");
+    system(command);
+
+    close(sockfd);
     return 0;
 }
